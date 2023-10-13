b87c60fdd663ce040cef1e266a4a564efe57f3ae
PARK JIMIN <yoseb7120@kangwon.ac.kr>
2023-10-13 10:01:53 +0900

    구구단 함수 완성

---
+++ index.js
@@ -0,0 +1,10 @@
+//구구단 출력 함수
+const makemultiplication = () => {
+for(let i = 1 ; i <= 9; i++) {
+    for(let j = 1; j <=9; j++) {
+        console.log(`${i} * ${j} = ${i*j}`)
+    }
+ }
+};
+
+makemultiplication();
